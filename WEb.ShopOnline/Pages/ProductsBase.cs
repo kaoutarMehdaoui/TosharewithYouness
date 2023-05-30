a933db1ed3ce1d663881731a016776b82e0b9f19
Kaoutar Mehdaoui <kmehdaoui@smb.sqli.com>
2023-05-30 11:35:52 +0100

    update_class_product_base

--- WEb.ShopOnline/Pages/ProductsBase.cs
+++ WEb.ShopOnline/Pages/ProductsBase.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using ShopOnline.Modes.DTOs;
+using System.Collections.Immutable;
 using WEb.ShopOnline.Services.Contracts;
 
 namespace WEb.ShopOnline.Pages
@@ -14,6 +15,20 @@ namespace WEb.ShopOnline.Pages
         {
             ProductFromApi = (await _productService.GetProducts()).ToList();
 
+
+        }
+        protected IOrderedEnumerable<IGrouping<string,ProductDTO>> GetGroupedProductsByCategory() 
+        {
+            var  result = from product in ProductFromApi group product by product.CategoryName into probyCatGroup orderby probyCatGroup.Key select  probyCatGroup ;
+            return result;
+        }
+        protected string GetCategoryName(IGrouping<string, ProductDTO> product)
+        {
+             
+
+             string categoryName = product.FirstOrDefault(ct=>ct.CategoryName== product.Key).CategoryName;
+            return categoryName;
+            
 
         }
     }
